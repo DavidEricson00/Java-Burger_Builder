bfe12d448a1967b40ca5b9f2c4960344db3a2550
David Ericson <davidestudofcg@gmail.com>
2024-10-27 16:35:37 -0300

    comentários e estrutura basica finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Main{
-	public static int input(int opcao) {
+	public static int input(int opcao) { // Classe do Input
 		Scanner input = new Scanner(System.in);
-		System.out.println("Qual opção você deseja?: ");
+		System.out.print("Qual opção você deseja?: ");
 		opcao = input.nextInt();
 		return opcao;
 	}
 	
-	public static void menuprincipal() {
+	public static void menuprincipal() { // Classe do Menu
 		System.out.println("MENU DE OPÇÕES:");
 		System.out.println("1. Pães");
 		System.out.println("2. Queijos");
@@ -18,14 +18,21 @@ public class Main{
 		System.out.println("6. Outros");
 		System.out.println("7. Sair");
 	}
+	
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // Classe Principal
 	int resposta = 0;
+
 		
 	while (resposta!=7) {
 		menuprincipal();
-		input(resposta);}
+		resposta = input(resposta);
+		}
 	
+	System.out.println("Até logo!");
 	}
+	
+	
 
 }
