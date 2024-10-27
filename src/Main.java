aa94606ae1d2019ba1caf740102b035d4e10dcd9
David Ericson <davidestudofcg@gmail.com>
2024-10-27 16:54:11 -0300

    matrizes dos cardapios

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,73 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
-	public static int input(int opcao) { // Classe do Input
-		Scanner input = new Scanner(System.in);
-		System.out.print("Qual opção você deseja?: ");
-		opcao = input.nextInt();
-		return opcao;
-	}
-	
-	public static void menuprincipal() { // Classe do Menu
-		System.out.println("MENU DE OPÇÕES:");
-		System.out.println("1. Pães");
-		System.out.println("2. Queijos");
-		System.out.println("3. Carnes");
-		System.out.println("4. Verduras");
-		System.out.println("5. Molhos");
-		System.out.println("6. Outros");
-		System.out.println("7. Sair");
-	}
-	
-	
+public class Main {
+    private static String[][] cardapio = {
+        {"Pão Francês", "Pão Carteira", "Pão de Hambúrguer", "Pão Árabe"},
+        {"Coalho", "Minas", "Muçarela", "Cream Cheese", "Gorgonzola"},
+        {"Mortadela", "Apresuntado", "Bacon", "Presunto", "Pepperoni", "Salame"},
+        {"Cebola", "Pimentão", "Tomate"},
+        {"Maionese", "Ketchup", "Maionese Temperada", "Molho Tártaro", "Barbecue"},
+        {"Batata Palha", "Ovo"}
+    };
 
-	public static void main(String[] args) { // Classe Principal
-	int resposta = 0;
+    private static double[][] precos = {
+        {0.25, 0.30, 0.70, 1.30},
+        {1.50, 1.80, 2.00, 3.00, 3.50},
+        {0.50, 1.00, 1.30, 1.60, 1.80, 2.00},
+        {0.30, 0.45, 0.50},
+        {0.50, 0.50, 0.70, 1.00, 1.50},
+        {1.00, 1.00}
+    };
+    
+    public static void menuprincipal() {
+        System.out.println("MENU DE OPÇÕES:");
+        System.out.println("1. Pães");
+        System.out.println("2. Queijos");
+        System.out.println("3. Carnes");
+        System.out.println("4. Verduras");
+        System.out.println("5. Molhos");
+        System.out.println("6. Outros");
+        System.out.println("7. Sair");
+    }
+    
+    public static void mostrarcardapio(int opcao) {
+    	if (opcao == 1) {
+    		System.out.println(Arrays.toString(cardapio[0]));
+    	}
+    	else if (opcao == 2) {
+    		
+    	}
+    	else if (opcao == 3) {
+    		
+    	}
+    	else if (opcao == 4) {
+    		
+    	}
+    	else if (opcao == 5) {
+    		
+    	}
+    	else if (opcao == 6) {
+    		
+    	}
+    }
 
-		
-	while (resposta!=7) {
-		menuprincipal();
-		resposta = input(resposta);
-		}
-	
-	System.out.println("Até logo!");
-	}
-	
-	
+    public static int input() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Qual opção você deseja?: ");
+        int opcao = input.nextInt();
+        return opcao;
+    }
 
+    public static void main(String[] args) {
+        int resposta = 0;
+
+        while (resposta != 7) {
+            menuprincipal();
+            resposta = input();
+            mostrarcardapio(resposta);
+        }
+
+        System.out.println("Até logo!");
+    }
 }
